7608e6ab428f41710563d0bc2dc79573c5a5729d
adamrajch <arajchwald@gmail.com>
2021-10-24 06:47:37 -0500

    boiler

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['@mantine/eslint-config'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'linebreak-style': 'off',
+  },
+};
---
+++ pages/_app.tsx
@@ -0,0 +1,39 @@
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  GlobalStyles,
+  MantineProvider,
+  NormalizeCSS,
+} from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { useState } from 'react';
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState('light');
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+  return (
+    <>
+      <Head>
+        <title>Mantine next example</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+        <MantineProvider
+          theme={{
+            colorScheme,
+          }}
+        >
+          <NormalizeCSS />
+          <GlobalStyles />
+          <NotificationsProvider>
+            <Component {...pageProps} />
+          </NotificationsProvider>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
+}
