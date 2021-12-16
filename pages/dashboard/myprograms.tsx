fa5678f209e3ea3636962af70b70432cb58de939
adamrajch <arajchwald@gmail.com>
2021-12-16 13:03:46 -0600

    cl

--- pages/dashboard/myprograms.tsx
+++ pages/dashboard/myprograms.tsx
@@ -109,6 +109,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       },
     };
   } catch (error) {
+    console.log(error);
     return {
       redirect: {
         destination: '/login',
