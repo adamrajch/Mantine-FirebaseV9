212512b034cb60325944902cc31916400c2f29e1
adamrajch <arajchwald@gmail.com>
2022-03-01 07:00:25 -0600

    mobile

--- components/auth/login.tsx
+++ components/auth/login.tsx
@@ -8,6 +8,7 @@ import React from 'react';
 import { AiOutlineGoogle } from 'react-icons/ai';
 import * as Yup from 'yup';
 import { auth } from '../../firebase';
+import HomePageContainter from '../homePages/HomePageContainter';
 const provider = new GoogleAuthProvider();
 const SignUpSchema = Yup.object().shape({
   email: Yup.string().min(2, 'Too Short!').max(40, 'Too Long!').required('Required'),
@@ -55,96 +56,99 @@ export default function Login(): JSX.Element {
   };
 
   return (
-    <Center style={{ width: '100%', height: '100%' }}>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={async (values) => {
-          // signinWithEmail(values.email, values.password);
-          signInWithEmailAndPassword(auth, values.email, values.password)
-            .then((userCredential) => {
-              // Signed in
-              const user = userCredential.user;
-              router.push('/dashboard');
-              // ...
-            })
-            .catch((error) => {
-              const errorCode = error.code;
-              const errorMessage = error.message;
+    <HomePageContainter>
+      <Center style={{ width: '100%', height: '100%' }}>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={async (values) => {
+            // signinWithEmail(values.email, values.password);
+            signInWithEmailAndPassword(auth, values.email, values.password)
+              .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                router.push('/dashboard');
+                // ...
+              })
+              .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
 
-              notifications.showNotification({
-                title: 'Login Error',
-                message: `${error.message}🤥`,
+                notifications.showNotification({
+                  title: 'Login Error',
+                  message: `${error.message}🤥`,
+                });
               });
-            });
-        }}
-        enableReinitialize={false}
-        validateOnChange={false}
-        validateOnBlur={false}
-        validationSchema={SignUpSchema}
-      >
-        {({ handleSubmit, handleChange, errors }) => (
-          <form onSubmit={handleSubmit}>
-            <Group
-              grow
-              position="center"
-              direction="column"
-              sx={(theme) => ({
-                padding: 16,
-                borderRadius: theme.radius.md,
-                minWidth: 500,
-                backgroundColor:
-                  theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.dark[1],
-                boxShadow: '6px 6px  14px   #0f0f0f, -2px -2px 6px #02455f',
-                '&:hover': {
-                  boxShadow: '12px 12px 16px  #0f0f0f, -2px -2px 6px #14698b',
-                },
+          }}
+          enableReinitialize={false}
+          validateOnChange={false}
+          validateOnBlur={false}
+          validationSchema={SignUpSchema}
+        >
+          {({ handleSubmit, handleChange, errors }) => (
+            <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+              <Group
+                grow
+                position="center"
+                direction="column"
+                sx={(theme) => ({
+                  padding: 16,
+                  borderRadius: theme.radius.md,
 
-                '@media (max-width: 755px)': {
-                  minWidth: 380,
-                },
-              })}
-            >
-              <SimpleGrid cols={1}>
-                <Title align="center">Login</Title>
-                <TextInput
-                  type="email"
-                  label="email"
-                  required
-                  name="email"
-                  onChange={handleChange}
-                  error={errors.email}
-                />
-                <TextInput
-                  type="password"
-                  label="password"
-                  required
-                  name="password"
-                  onChange={handleChange}
-                  error={errors.password}
-                />
-                <Group position="apart">
-                  <NextLink href="/signup">
-                    <Text size="sm" style={{ cursor: 'pointer' }}>
-                      Create an account
-                    </Text>
-                  </NextLink>
-                  <NextLink href="/forgot-password">
-                    <Text size="sm" style={{ cursor: 'pointer' }}>
-                      Forgot Password?
-                    </Text>
-                  </NextLink>
-                </Group>
-                <Button variant="outline" type="submit">
-                  Submit
-                </Button>
-                <Group>
-                  <SignInButton />
-                </Group>
-              </SimpleGrid>
-            </Group>
-          </form>
-        )}
-      </Formik>
-    </Center>
+                  // minWidth: 500,
+                  backgroundColor:
+                    theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.dark[1],
+                  boxShadow: '6px 6px  14px   #0f0f0f, -2px -2px 6px #02455f',
+                  '&:hover': {
+                    boxShadow: '12px 12px 16px  #0f0f0f, -2px -2px 6px #14698b',
+                  },
+
+                  // '@media (max-width: 755px)': {
+                  //   minWidth: 380,
+                  // },
+                })}
+              >
+                <SimpleGrid cols={1}>
+                  <Title align="center">Login</Title>
+                  <TextInput
+                    type="email"
+                    label="email"
+                    required
+                    name="email"
+                    onChange={handleChange}
+                    error={errors.email}
+                  />
+                  <TextInput
+                    type="password"
+                    label="password"
+                    required
+                    name="password"
+                    onChange={handleChange}
+                    error={errors.password}
+                  />
+                  <Group position="apart">
+                    <NextLink href="/signup">
+                      <Text size="sm" style={{ cursor: 'pointer' }}>
+                        Create an account
+                      </Text>
+                    </NextLink>
+                    <NextLink href="/forgot-password">
+                      <Text size="sm" style={{ cursor: 'pointer' }}>
+                        Forgot Password?
+                      </Text>
+                    </NextLink>
+                  </Group>
+                  <Button variant="outline" type="submit">
+                    Submit
+                  </Button>
+                  <Group>
+                    <SignInButton />
+                  </Group>
+                </SimpleGrid>
+              </Group>
+            </form>
+          )}
+        </Formik>
+      </Center>
+    </HomePageContainter>
   );
 }
--- components/auth/signup.tsx
+++ components/auth/signup.tsx
@@ -8,6 +8,7 @@ import React, { ReactElement } from 'react';
 import { AiOutlineGoogle } from 'react-icons/ai';
 import * as Yup from 'yup';
 import { auth } from '../../firebase';
+import HomePageContainter from '../homePages/HomePageContainter';
 
 const SignUpSchema = Yup.object().shape({
   email: Yup.string().min(2, 'Too Short!').max(40, 'Too Long!').required('Required'),
@@ -50,93 +51,90 @@ export default function SignUp({}: any): ReactElement {
   }
 
   return (
-    <Center style={{ height: '100%', width: '100%' }}>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={async (values) => {
-          createUserWithEmailAndPassword(auth, values.email, values.password)
-            .then((userCredential) => {
-              router.push('/dashboard');
-            })
-            .catch((error) => {
-              const errorCode = error.code;
-              const errorMessage = error.message;
-              console.log(errorMessage, errorCode);
-              notifications.showNotification({
-                title: 'Registration Error',
-                message: `${error.message}🤥`,
+    <HomePageContainter>
+      <Center style={{ height: '100%', width: '100%' }}>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={async (values) => {
+            createUserWithEmailAndPassword(auth, values.email, values.password)
+              .then((userCredential) => {
+                router.push('/dashboard');
+              })
+              .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage, errorCode);
+                notifications.showNotification({
+                  title: 'Registration Error',
+                  message: `${error.message}🤥`,
+                });
+                return errorMessage;
+                // ..
               });
-              return errorMessage;
-              // ..
-            });
-        }}
-        enableReinitialize={false}
-        validateOnChange={false}
-        validateOnBlur={false}
-        validationSchema={SignUpSchema}
-      >
-        {({ handleSubmit, handleChange, errors }) => (
-          <form onSubmit={handleSubmit}>
-            <Group
-              grow
-              position="center"
-              direction="column"
-              sx={(theme) => ({
-                padding: 16,
-                borderRadius: theme.radius.md,
-                minWidth: 500,
-                backgroundColor:
-                  theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.dark[1],
-                boxShadow: '6px 6px  14px   #0f0f0f, -2px -2px 6px #02455f',
-                '&:hover': {
-                  boxShadow: '12px 12px 16px  #0f0f0f, -2px -2px 6px #14698b',
-                },
+          }}
+          enableReinitialize={false}
+          validateOnChange={false}
+          validateOnBlur={false}
+          validationSchema={SignUpSchema}
+        >
+          {({ handleSubmit, handleChange, errors }) => (
+            <form onSubmit={handleSubmit}>
+              <Group
+                grow
+                position="center"
+                direction="column"
+                sx={(theme) => ({
+                  padding: 16,
+                  borderRadius: theme.radius.md,
+                  backgroundColor:
+                    theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.dark[1],
+                  boxShadow: '6px 6px  14px   #0f0f0f, -2px -2px 6px #02455f',
+                  '&:hover': {
+                    boxShadow: '12px 12px 16px  #0f0f0f, -2px -2px 6px #14698b',
+                  },
+                })}
+              >
+                <Title align="center">Sign Up</Title>
+                <TextInput
+                  type="email"
+                  label="email"
+                  required
+                  name="email"
+                  onChange={handleChange}
+                  error={errors.email}
+                />
+                <TextInput
+                  type="password"
+                  label="password"
+                  required
+                  name="password"
+                  onChange={handleChange}
+                  error={errors.password}
+                />
+                <Group position="apart">
+                  <NextLink href="/login">
+                    <Text size="sm" style={{ cursor: 'pointer' }}>
+                      Have account? Login
+                    </Text>
+                  </NextLink>
 
-                '@media (max-width: 755px)': {
-                  minWidth: 380,
-                },
-              })}
-            >
-              <Title align="center">Sign Up</Title>
-              <TextInput
-                type="email"
-                label="email"
-                required
-                name="email"
-                onChange={handleChange}
-                error={errors.email}
-              />
-              <TextInput
-                type="password"
-                label="password"
-                required
-                name="password"
-                onChange={handleChange}
-                error={errors.password}
-              />
-              <Group position="apart">
-                <NextLink href="/login">
-                  <Text size="sm" style={{ cursor: 'pointer' }}>
-                    Have account? Login
-                  </Text>
-                </NextLink>
-
-                <NextLink href="/forgot-password">
-                  <Text size="sm" style={{ cursor: 'pointer' }}>
-                    Forgot Password?
-                  </Text>
-                </NextLink>
+                  <NextLink href="/forgot-password">
+                    <Text size="sm" style={{ cursor: 'pointer' }}>
+                      Forgot Password?
+                    </Text>
+                  </NextLink>
+                </Group>
+                <Button variant="outline" type="submit">
+                  Submit
+                </Button>
+                <Group>
+                  <SignInButton />
+                </Group>
               </Group>
-              <Button variant="outline" type="submit">
-                Submit
-              </Button>
-              <Group>
-                <SignInButton />
-              </Group>
-            </Group>
-          </form>
-        )}
-      </Formik>
-    </Center>
+            </form>
+          )}
+        </Formik>
+      </Center>
+    </HomePageContainter>
   );
 }
